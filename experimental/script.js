29195ccbed13a560096a6e6861faae7a1d380385
Matthias Jenny <matthiascjenny@gmail.com>
2017-12-16 02:55:00 -0500

    More styling

--- experimental/script.js
+++ experimental/script.js
@@ -1,3 +1,10 @@
 const homeContent = `
   Hello
 `;
+
+document.addEventListener("DOMContentLoaded", function() {
+  const main = Array.from(document.getElementsByTagName('main'));
+  setTimeout(() => {
+    main.forEach((el) => el.setAttribute("style", "opacity: .85"));
+  }, 100 );
+});
