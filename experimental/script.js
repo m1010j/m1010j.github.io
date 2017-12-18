59206becb7b5ea9c8d2fbf023cfdb55b36634de1
Matthias Jenny <matthiascjenny@gmail.com>
2017-12-18 10:00:33 -0500

    Use webpack to transpile JS

--- experimental/script.js
+++ experimental/script.js
@@ -1,48 +1,108 @@
-const contents = {
-  home: `
-    Hello Home
-  `,
-  projects: `
-    <a href="mailto:matthiascjenny@gmail.com">Hello Projects</a>
-  `,
-  contact: `
-    Hello Contact
-  `,
-  resume: `
-    <center><iframe
-    src="https://docs.google.com/presentation/d/e/2PACX-1vSvbIgZ_s_z-P2f-WrCXbGA4-nrTgCfU6Zr7ozlHnoViAZ92P2syQZyh22_ZTlmtW2dCbyRHFdcNotw/embed?start=false&loop=false&delayms=1000"
-    frameborder="0"
-    allowfullscreen="true"
-    mozallowfullscreen="true"
-    webkitallowfullscreen="true">
-    </iframe></center>
-  `,
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var contents = {
+  home: '\n    Hello Home\n  ',
+  projects: '\n    <a href="mailto:matthiascjenny@gmail.com">Hello Projects</a>\n  ',
+  contact: '\n    Hello Contact\n  ',
+  resume: '\n    <center><iframe\n    src="https://docs.google.com/presentation/d/e/2PACX-1vSvbIgZ_s_z-P2f-WrCXbGA4-nrTgCfU6Zr7ozlHnoViAZ92P2syQZyh22_ZTlmtW2dCbyRHFdcNotw/embed?start=false&loop=false&delayms=1000"\n    frameborder="0"\n    allowfullscreen="true"\n    mozallowfullscreen="true"\n    webkitallowfullscreen="true">\n    </iframe></center>\n  '
 };
 
-document.addEventListener('DOMContentLoaded', function() {
-  const main = Array.from(document.getElementsByTagName('main'));
-  const navItems = Array.from(document.getElementsByClassName('navitem'));
-  const content = document.getElementById('content');
-  setTimeout(() => {
-    main.forEach((el) => el.setAttribute('style', 'opacity: .85'));
+document.addEventListener('DOMContentLoaded', function () {
+  var main = Array.from(document.getElementsByTagName('main'));
+  var navItems = Array.from(document.getElementsByClassName('navitem'));
+  var content = document.getElementById('content');
+  setTimeout(function () {
+    main.forEach(function (el) {
+      return el.setAttribute('style', 'opacity: .85');
+    });
     content.innerHTML = contents.home;
-  }, 100 );
+  }, 100);
 
-  navItems.forEach((navItem) => {
+  navItems.forEach(function (navItem) {
     navItem.addEventListener('click', toggleActive(navItems, content));
   });
 });
 
-const toggleActive = (navItems, content) => {
-  return (e) => {
-    navItems.forEach((navItem) => {
-      const type = navItem.innerText.toLowerCase();
+var toggleActive = function toggleActive(navItems, content) {
+  return function (e) {
+    navItems.forEach(function (navItem) {
+      var type = navItem.innerText.toLowerCase();
       if (e.target === navItem) {
-        navItem.classList.add(`${type}-active`);
+        navItem.classList.add(type + '-active');
         navItem.classList.remove(type);
         content.innerHTML = contents[type];
       } else {
-        navItem.classList.remove(`${type}-active`);
+        navItem.classList.remove(type + '-active');
         navItem.classList.add(type);
       }
     });
@@ -50,3 +110,7 @@ const toggleActive = (navItems, content) => {
 };
 // width="100%"
 // height="100%"
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=script.js.map
---
+++ experimental/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: './entry.js',
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'script.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '*']
+  }
+};
