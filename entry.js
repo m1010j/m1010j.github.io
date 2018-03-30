6c7326308436849e3de0009c6de95387f22bd8cf
Matthias Jenny <matthiascjenny@gmail.com>
2018-03-30 16:41:45 -0400

    Update resume

--- entry.js
+++ entry.js
@@ -57,7 +57,7 @@ const contents = {
   `,
   resume: `
   <div class="resume"><p><a href="./Matthias_Jenny_Resume.pdf"><i class="fa fa-download" aria-hidden="true"></i> Download resume</a></p>
-    <iframe src="http://docs.google.com/gview?embedded=true&url=http://www.matthiasjenny.com/Matthias_Jenny_Resume.pdf" type="application/pdf" />
+    <iframe src="./resume/index.html"></iframe>
   </div>
   `,
   contact: `
